57dfe819020c8dc642c17482db1894fb8908cd19
Barry <barry@citypolyhs.org>
2017-01-11 21:24:08 +0000

    i changed the color

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //Write your function calls below this line
+setColor(0,0,"purple");
+setColor(0,9, "red");
+setColor(9,0,"blue");
+setColor(9,9,"green");
+
+
+setColor(0,3,"yellow");
+setColor(0,6,"yellow");
+setColor(2,4,"yellow");
+setColor(2,5,"yellow");
+setColor(4,4,"yellow"),
+setColor(4,5,"yellow");
+setColor(2,1,"red");
+setColor(2,0,"red");
+setColor(2,2,"red")
+setColor(3,0,"red");
+setColor(4,1,"red");
+setColor(4,2,"red");
+setColor(5,2,"red");
+setColor(6,2,"red");
+setColor(6,1,"red");
+setColor(6,0,"red");
+setColor(4,0,"red");
+setColor(6,4,"red");
+setColor(7,4,"red");
+setColor(6,5,"yellow");
+setColor(7,5,"yellow");
+setColor(6,6,"green ");
+setColor(7,6,"green");
+
+
